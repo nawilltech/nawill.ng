a0ad4bf1c2d581ce65ba9e6f2d1dc8d35d95d7fc
Ushahemba Shir <laleoict@gmail.com>
2022-01-21 01:41:44 +0100

    index menu change to SERVICES, OUR WORKS, COMPANY and CONTACT

--- pages/consultation.php
+++ pages/consultation.php
@@ -4,20 +4,34 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'Contact us at nawill.ng <info@nawill.ng>';
+$from = 'Consultation session nawill.ng <info@nawill.ng>';
 
 // an email address that will receive the email with the output of the form
-$sendTo = 'Contact us at nawill.ng <info@nawill.ng>';
+$sendTo = 'Consultation session at nawill.ng <info@nawill.ng>';
 
 // subject of the email
-$subject = 'New message from contact form';
+$subject = 'New Consultation request form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
+$fields = array( 
+    'about' => 'About',
+    'service' => 'Service',
+    'schedule' => 'Schedule',
+    'budget' => "Budget",
+    'fullname' => 'Fullname',
+    'email' => 'Email',
+    'phone_number' => 'Phone', 
+    'company' => 'Company', 
+    'role' => 'Role', 
+    'accessibility' => 'Accessibility'
+    ); 
+
+// fields 
 
 // message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+$homePage = 'nawill.ng';
+$okMessage = 'Consultation form successfully submitted. Thank you, We will get back to you soon!';
 
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
@@ -31,28 +45,22 @@ error_reporting(E_ALL & ~E_NOTICE);
 
 try
 {
-
-    if(count($_POST) == 0) throw new \Exception('Form is empty');
-            
-    $emailText = "You have a new message from your contact form\n=============================\n";
-
+    if(count($_POST) == 0) throw new \Exception('Form is empty');      
+    $emailText = "You have a new Consultation Request from nawill.ng\n=============================\n";
     foreach ($_POST as $key => $value) {
         // If the field exists in the $fields array, include it in the email 
         if (isset($fields[$key])) {
             $emailText .= "$fields[$key]: $value\n";
         }
     }
-
     // All the necessary headers for the email.
     $headers = array('Content-Type: text/plain; charset="UTF-8";',
         'From: ' . $from,
         'Reply-To: ' . $_POST['email'],
         'Return-Path: ' . $from,
     );
-    
     // Send email
     mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
     $responseArray = array('type' => 'success', 'message' => $okMessage);
 }
 catch (\Exception $e)
@@ -60,13 +68,10 @@ catch (\Exception $e)
     $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
 
-
 // if requested by AJAX request return JSON response
 if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
-
     header('Content-Type: application/json');
-
     echo $encoded;
 }
 // else just display the message
